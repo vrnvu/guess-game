2f1a3c4d6fc8277e78a608339ee4cd440ad3e7ad
Arnau Díaz <diazarnau0@gmail.com>
2020-04-18 15:36:46 +0200

    game state struct

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,45 @@
 use rand::Rng;
 use std::cmp::Ordering;
 
+struct GameState {
+    secret: u32,
+    from_range: u32,
+    to_range: u32,
+    tries: u32,
+    playing: bool,
+}
+
 fn main() {
-    let secret = gen_secret(1, 101);
+    let mut game_state = GameState {
+        secret: gen_secret(1, 101),
+        from_range: 1,
+        to_range: 100,
+        tries: 0,
+        playing: true,
+    };
 
-    let mut from_range = 1;
-    let mut to_range = 100;
-    let mut tries = 0;
+    while game_state.playing {
+        game_state.tries += 1;
+        let guess = guess_number(game_state.from_range, game_state.to_range);
+        let comparison = guess.cmp(&game_state.secret);
 
-    loop {
-        tries += 1;
-        let guess = guess_number(from_range, to_range);
-        let comparison = guess.cmp(&secret);
         match comparison {
             Ordering::Less => {
-                from_range = update_from_range(from_range, guess);
+                game_state.from_range = update_from_range(game_state.from_range, guess);
                 println!("Too small!");
             }
             Ordering::Greater => {
-                to_range = update_to_range(to_range, guess);
+                game_state.to_range = update_to_range(game_state.to_range, guess);
                 println!("Too big!");
             }
             Ordering::Equal => {
                 println!("Found it!");
-                break;
+                game_state.playing = false;
             }
         }
     }
-    println!("Found {} in {} tries", secret, tries);
+
+    println!("Found {} in {} tries", game_state.secret, game_state.tries);
 }
 
 fn update_from_range(from_range: u32, guess: u32) -> u32 {
